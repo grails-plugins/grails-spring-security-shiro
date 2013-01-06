75f2194ccaac996468e3d48ae6720020bce98092
Burt Beckwith <beckwithb@vmware.com>
2013-01-06 04:45:19 -0500

    added docs

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
-grails.project.target.level = 1.6
+grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
