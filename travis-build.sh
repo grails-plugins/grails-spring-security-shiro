5e8109ba9c5e33e89af02a8178dc7abd09e306ce
Puneet Behl <behl4485@gmail.com>
2018-04-11 01:35:44 +0530

    #4 Added echo statements to travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -6,14 +6,17 @@ rm -rf build
 
 ./gradlew -q clean check install --stacktrace
 
- ./integration-test-app/run_integration_tests.sh
+echo "Running Integration tests ... "
+./integration-test-app/run_integration_tests.sh
 
 echo "branch: $TRAVIS_BRANCH"
-echo "pullrequest: $TRAVIS_PULL_REQUEST"
+echo "pull-request: $TRAVIS_PULL_REQUEST"
 echo "travis tag: $TRAVIS_TAG"
 
 if [[ -n $TRAVIS_TAG ]] && [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    
+
+    echo "Publishing archives ... "
+
     ./gradlew bintrayUpload --stacktrace
 
     ./publish-docs.sh
