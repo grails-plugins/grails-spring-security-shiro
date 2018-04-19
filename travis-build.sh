e687670b5904ba1800cc61b62f68fbe9cb4db925
Puneet Behl <behl4485@gmail.com>
2018-04-19 17:59:18 +0530

    Updated Travis build script to publish snapshot and docs when tag is not present.

--- travis-build.sh
+++ travis-build.sh
@@ -6,15 +6,24 @@ rm -rf build
 
 ./gradlew -q clean check install --stacktrace
 
- ./integration-test-app/run_integration_tests.sh
+EXIT_STATUS=0
+
+echo "Running Integration tests ... "
+./integration-test-app/run_integration_tests.sh
 
 echo "branch: $TRAVIS_BRANCH"
-echo "pullrequest: $TRAVIS_PULL_REQUEST"
+echo "pull-request: $TRAVIS_PULL_REQUEST"
 echo "travis tag: $TRAVIS_TAG"
 
-if [[ -n $TRAVIS_TAG ]] && [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    
-    ./gradlew bintrayUpload --stacktrace
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+    echo "Publishing archives ... "
+
+    if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+    else
+      ./gradlew publish || EXIT_STATUS=$?
+    fi
 
     ./publish-docs.sh
 
